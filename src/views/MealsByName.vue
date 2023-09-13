3a9aaf4d52a6378914434315bc304000c0291f59
iraklikeche <irakli.kechee@gmail.com>
2023-09-13 14:47:40 +0400

    layout for MealsByName

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -8,15 +8,46 @@
       @change="searchMeals"
     />
   </div>
-  <div>
-    <pre>{{ meals }}</pre>
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+    <div
+      v-for="meal of meals"
+      :key="meal.idMeal"
+      class="bg-white shadow rounded-xl"
+    >
+      <router-link to="/">
+        <img
+          :src="meal.strMealThumb"
+          :alt="meal.strMeal"
+          class="rounded-t-xl w-full h-48 object-cover"
+        />
+      </router-link>
+      <div class="p-3">
+        <h3 class="font-bold">{{ meal.strMeal }}</h3>
+        <p class="mb-5">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, autem!
+        </p>
+        <div class="flex items-center justify-between">
+          <a
+            :href="meal.strYoutube"
+            target="_blank"
+            class="px-3 py-2 rounded border-2 text-white border-red-600 bg-red-500 hover:bg-red-600 transition-colors"
+          >
+            Youtube Link
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, onMounted } from "vue";
 import { useMealsStore } from "../stores/meals";
 import axios from "axios";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
 const mealStore = useMealsStore();
 
 const keyword = ref("");
@@ -31,4 +62,15 @@ const searchMeals = async () => {
 
   mealStore.setSearchResults(response.data.meals);
 };
+
+onMounted(async () => {
+  try {
+    keyword.value = route.params.name;
+    if (keyword.value) {
+      await searchMeals();
+    }
+  } catch (error) {
+    console.error("Error during onMounted:", error);
+  }
+});
 </script>
